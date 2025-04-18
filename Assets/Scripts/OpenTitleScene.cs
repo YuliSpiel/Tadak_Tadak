f0051dbb1475fca631e4d6a70f2a3444664d0604
rnrgll <shuajjjjj@naver.com>
2025-04-18 23:58:55 +0900

    [Feat]: 게임 오버 / 게임 클리어 ui 및 씬 연결 완료

--- Assets/Scripts/OpenTitleScene.cs
+++ Assets/Scripts/OpenTitleScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // 게임종료 팝업의 'Back to Title' 버튼에 부착
 public class OpenTitleScene : MonoBehaviour
@@ -9,5 +10,10 @@ public class OpenTitleScene : MonoBehaviour
     {
         GameManager.Instance.LoadTitleScene();
         Time.timeScale = 1;
+    }
+
+    public void LoadEndScene()
+    {
+        SceneManager.LoadScene("3_EndScene");
     }
 }
