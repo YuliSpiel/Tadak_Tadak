db1d79155a3bd67c75e0106374d76f6fd84b3b54
Jiyun <juliyooni@gmail.com>
2025-04-19 13:21:25 +0900

    [Fix]: 미세수정

--- Assets/Resources/2_CatGame/Scripts/CheckPoint.cs
+++ Assets/Resources/2_CatGame/Scripts/CheckPoint.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private CatGame _game;
+    [SerializeField] private GameObject _successPanel;
+
+    void Start()
+    {
+        _successPanel.SetActive(false);
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Cat"))
         {
             SoundManager.Instance.PlaySFX(ESFXs.Cat3SFX);
             SoundManager.Instance.PlaySFX(ESFXs.GrowSFX);
-
+            _successPanel.SetActive(true);
+            
             Destroy(gameObject);
             GameManager.Instance.Score += 10;
-            _game.Invoke("EndGame", 3f);
+            _game.Invoke("EndGame", 1.5f);
         }
     }
 }
